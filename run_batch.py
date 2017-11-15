a71b1d69d382e29aa10f0c506bebeda57c0ccf95
Ming Li <mingyli@berkeley.edu>
2017-11-15 09:34:26 -0800

    created automated runner

---
+++ run_batch.py
@@ -0,0 +1,8 @@
+
+import subprocess
+import os
+
+# first positional argument is 20, 35, or 50. second position argument is the input file number
+cmd = "python3 solver.py phase2_inputs/inputs{0}/input{0}_{1}.in outputs/outputs{0}/output{0}_{1}.out > outputs/outputs{0}/output{0}_{1}.log" 
+
+subprocess.run(cmd.format(20, 5), shell=True)
