e117839e48b892f09086399e3fe5c7bd470ba1f5
ming li <mingyli34@gmail.com>
2017-11-19 15:55:41 -0800

    added kick to swap solver

--- solver2.py
+++ solver2.py
@@ -7,11 +7,11 @@ import itertools
 from itertools import combinations
 
 
+# for use when keyboard interrupt. 
+best_state = (float("inf"), None)
 
 def solve(num_wizards, num_constraints, wizards, constraints):
-
-    # mapping from wizard name to index
-    wizard_index = {wizard: i for i, wizard in enumerate(wizards)}
+    global best_state
 
     def is_conflict(constraint):
         wi, wj, wk = constraint    
@@ -21,39 +21,57 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     num_conflicts = lambda: sum(is_conflict(constraint) for constraint in constraints)
 
     def swap(i, j):
+        wizard_index[wizards[i]], wizard_index[wizards[j]] = wizard_index[wizards[j]], wizard_index[wizards[i]]
         wizards[i], wizards[j] = wizards[j], wizards[i]
 
+    def kick(strength=3):
+        for _ in range(strength):
+            i, j = random.sample(range(num_wizards), 2)
+            swap(i, j)
+            
+
+    random.shuffle(wizards)
+    wizard_index = {wizard: i for i, wizard in enumerate(wizards)}
+
     for _ in itertools.count():
+
         print("=============")
         print("iteration", _)
         print("conflicts", sum(is_conflict(constraint) for constraint in constraints))
 
-        n = 9
+        n = 7
         least_conflicts = []
 
         conflicts = num_conflicts()
         if 0 == conflicts:
             return wizards
+        if conflicts < best_state[0]:
+            best_state = (conflicts, wizards.copy())
 
         for i, j in itertools.combinations(range(num_wizards), 2):
             swap(i, j)
-            wizard_index = {wizard: i for i, wizard in enumerate(wizards)}
-
-            conflicts = num_conflicts()
+            new_conflicts = num_conflicts()
 
             # terminate early
-            if 0 == conflicts:
+            if 0 == new_conflicts:
                 return wizards
-
-            if len(least_conflicts) == n:
-                heapq.heappushpop(least_conflicts, (-conflicts, i, j))
-            else:
-                heapq.heappush(least_conflicts, (-conflicts, i, j))
 
             swap(i, j)
 
-        _, i, j = random.choice(least_conflicts)
-        swap(i, j)
+            if len(least_conflicts) == n:
+                heapq.heappushpop(least_conflicts, (-new_conflicts, i, j))
+            else:
+                heapq.heappush(least_conflicts, (-new_conflicts, i, j))
+
+
+        # if stagnant then kick
+        # if all(least_conflicts[0][0] == t[0] for t in least_conflicts):
+        if all(-conflicts == t[0] for t in least_conflicts):
+            kick(strength=4)
+            print("kicked at conflicts", conflicts)
+        else:
+            _, i, j = random.choice(least_conflicts)
+            swap(i, j)
     return wizards
 
 
@@ -89,7 +107,10 @@ if __name__=="__main__":
     try:
         solution = solve(num_wizards, num_constraints, wizards, constraints)
     except KeyboardInterrupt:
-        print("instance halted early. best state below.")
+        print("instance halted early.")
+        solution = best_state[1]
 
-    print("solution:", solution)
     write_output(args.output_file, solution)
+    print("best state below.")
+    print(best_state)
+    print("solution:", solution)
